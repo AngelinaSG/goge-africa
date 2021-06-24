f6aa5f9302b6d1d195337e8ef493bb9f7a17320a
unknown <smmlinchik@gmail.com>
2021-06-24 14:32:12 +0300

    add delivery form

---
+++ src/components/Cart/DeliveryInfo.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="row delivery-info__row">
+    <form class="delivery-info__form">
+      <h5 class="bold text-center">Select Your Delivery Information</h5>
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Full Name" />
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Country / Region"
+        />
+        <input type="text" class="form-control" placeholder="Street Adress" />
+        <input type="text" class="form-control" placeholder="City" />
+        <input
+          type="text"
+          class="form-control"
+          placeholder="State / Province / Region"
+        />
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Zip / Postal Code"
+        />
+        <input type="tel" class="form-control" placeholder="Mobile" />
+        <button class="btn btn--gradient-bg delivery__btn">
+          Save and Ship To This Address
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "~@/assets/scss/_custom.scss";
+
+.delivery-info__row {
+  padding-left: 78px;
+  padding-right: 121px;
+  padding-bottom: 134px;
+}
+
+.delivery-info__form {
+  width: 100%;
+  background: #fff7df;
+  border: 1px solid #efefef;
+  border-radius: 10px;
+  padding: 67px 0;
+}
+
+.delivery-info__form > h5 {
+  margin-bottom: 55px;
+}
+
+.form-group {
+  width: 57.2%;
+  margin: auto;
+}
+
+.form-control {
+  font-size: 0.875rem;
+  border: 1px solid #efefef;
+  border-radius: 100px;
+  height: 60px;
+  margin-bottom: 9px;
+  padding: 21px 0 21px 20px;
+  font-family: $font-family-base;
+}
+
+.form-control:last-of-type {
+  margin-bottom: 0;
+}
+
+.form-control::placeholder {
+  font-size: $font-size-base;
+  font-family: $font-family-base;
+  padding: 21px 0 21px 20px;
+}
+
+.delivery__btn {
+  width: 100%;
+  margin-top: 79px;
+}
+</style>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -7,57 +7,30 @@
           Back
         </a>
       </div>
-      <div class="row cart__table-row">
-        <div class="product-table">
-          <div class="cart__table-title">Product Name & Details</div>
-          <div class="cart__table-title">Quantity</div>
-          <div class="cart__table-title">Price</div>
-          <div class="cart__table-title"></div>
-          <div class="cart__table-title"></div>
-          <div class="cart__table-title">Product Name & Details</div>
-          <div class="cart__table-title">Quantity</div>
-          <div class="cart__table-title">Price</div>
-          <div class="cart__table-title"></div>
-          <div class="cart__table-title"></div>
-        </div>
-        <div class="cart__total-price">
-          <p class="text-center">Total: $480</p>
-          <button class="btn btn--gradient-bg">Buy All</button>
-        </div>
-      </div>
+
+      <CartTable />
+
+      <DeliveryInfo />
     </div>
   </div>
 </template>
 
 <script>
 import IconBack from "@/components/icons/IconBack";
+import CartTable from "@/components/Cart/CartTable";
+import DeliveryInfo from "@/components/Cart/DeliveryInfo";
 
 export default {
   name: "Cart",
   components: {
     IconBack,
+    CartTable,
+    DeliveryInfo,
   },
 };
 </script>
 
 <style scoped>
-.cart__table-row {
-  padding-left: 78px;
-  padding-right: 121px;
-  padding-bottom: 100px;
-  flex-direction: column;
-}
-
-.product-table {
-  width: 100%;
-  background: #fff7df;
-  border: 1px solid #efefef;
-  border-radius: 10px;
-  display: grid;
-  grid-template-columns: repeat(5, 260px);
-  margin-bottom: 65px;
-}
-
 .cart__link-back-icon {
   margin-right: 20px;
 }
@@ -65,19 +38,5 @@ export default {
 .cart__link-row {
   padding-top: 45px;
   padding-bottom: 29.3px;
-}
-
-.cart__table-title {
-  border-bottom: 1px solid #efefef;
-  padding: 39px 0 29px 39px;
-}
-
-.cart__total-price {
-  width: max-content;
-  align-self: flex-end;
-}
-
-.cart__total-price > p {
-  margin-bottom: 37px;
 }
 </style>
